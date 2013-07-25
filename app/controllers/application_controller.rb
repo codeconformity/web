812ce76c09630757edb7da6164be4d7ed21ba369
Alex Soulim <soulim@gmail.com>
2013-07-25 09:19:33 +0400

    Fix current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  private
+
   def current_user
-    @current_user ||= super.tap { |user| user.decorate if user.present? }
+    @current_user ||= begin
+      user = super
+      user.decorate if user
+    end
   end
 end
